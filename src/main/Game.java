fe05fa5830f1077d2ff469722caa20f75b8a538c
2390858916 <2390858916@qq.com>
2023-02-10 15:00:47 +0800

    游戏循环

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,13 +1,47 @@
 package main;
 
-public class Game {
+public class Game implements Runnable{
     private Gamewindow gamewindow;
     private GamePanel gamePanel;
+    //单独运行线程
+    private Thread gameThread;
+    private final int PFS_SET = 120;
     public Game(){
         gamePanel = new GamePanel();
         gamewindow = new Gamewindow(gamePanel);
         //必须添加 不然不能触发方法
         gamePanel.setFocusable(true);
         gamePanel.requestFocus();
+        startGameLoop();
+    }
+    //刷新画面
+    private void startGameLoop(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    //固定120帧
+
+    @Override
+    public void run() {
+        double tomePerFrame = 1000000000.0/PFS_SET;
+        long lastFrame = System.nanoTime();
+        long now = System.nanoTime();
+        int frames = 0;
+        long lastCheck = System.currentTimeMillis();
+        while (true){
+            now = System.nanoTime();
+            if(now - lastFrame >= tomePerFrame){
+                gamePanel.repaint();
+                lastFrame = now;
+                frames++;
+            }
+
+            if(System.currentTimeMillis() - lastCheck >= 1000){
+                lastCheck =  System.currentTimeMillis();
+                System.out.println("fps0" + frames);
+                frames=0;
+            }
+        }
     }
 }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -7,11 +7,19 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Random;
 
 public class GamePanel extends JPanel {
     private MouseInputs mouseInputs;
-    private int xDelta = 100,yDelta = 100;
+    private float xDelta = 100,yDelta = 100;
+    private float xDir = 1f,yDir = 1f;
+    private int frames = 0;
+    private long lastCheck = 0;
+    private Color color = new Color(150,90,60);
+    //随机颜色
+    private Random random;
     public GamePanel(){
+        random = new Random();
         mouseInputs = new MouseInputs(this);
         addKeyListener(new Keyboardinputs(this));
         addMouseListener(mouseInputs);
@@ -36,6 +44,29 @@ public class GamePanel extends JPanel {
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        g.fillRect(xDelta,yDelta,200,50);
+
+        updateRectangle();
+        g.setColor(color);
+        g.fillRect((int) xDelta,(int) yDelta,200,50);
+
+
+    }
+    //防止绘画图形超过窗口大小
+    public void updateRectangle(){
+        xDelta+= xDir;
+        if(xDelta > 400 || xDelta <0){
+            xDir*=-1;
+            color =getRndColor();
+        }
+        yDelta+=yDir;
+        if(yDelta > 400 || yDelta <0){
+            yDir*=-1;
+            color = getRndColor();
+        }
+    }
+    private Color getRndColor(){
+        int r=random.nextInt(255),b=random.nextInt(255),
+                g=random.nextInt(255);
+        return new Color(r,b,g);
     }
 }
